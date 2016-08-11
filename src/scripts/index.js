9f8b9c1c3f79cc7b062d08eb4a15ddbbd93dc45d
owen cowdry <owencowdry@gmail.com>
2016-08-11 13:11:49 +0100

    Got basic split build config working

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,10 @@
-require('../styles/index.styl'); // The page is now styled
+// require('../styles/index.styl'); // The page is now styled
 
-var exampleAdder = require('./example-adder.js');
+// var exampleAdder = require('./example-adder.js');
 
-let test = exampleAdder(5);
-console.log('test(5) => ', test(5) );
-console.log('test(10) => ', test(10) );
-console.log('test(20) => ', test(20) );
+// let test = exampleAdder(5);
+// console.log('test(5) => ', test(5) );
+// console.log('test(10) => ', test(10) );
+// console.log('test(20) => ', test(20) );
+
+console.log('scripts loaded!');
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+// Generate our index.html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Allows us to branch out config based on the run mode we are in
+const merge = require('webpack-merge');
+// Validation for the config file
+const validate = require('webpack-validator');
+// Plugin specific configuration
+const parts = require('./libs/parts');
+// Setup PATHS object
+const PATHS = {
+	entry : path.join(__dirname, 'src/scripts'),
+	markup : path.join(__dirname, 'src/markup/'),
+	build: path.join(__dirname, 'build')
+};
+// Setup common object
+const common = {
+	// Entry accepts a path or an object of entries.
+	// We'll be using the latter form given it's
+	// convenient with more complex configurations.
+	entry: {
+		app: PATHS.entry
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[name].js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack Core Base Setup',
+			template: PATHS.markup + 'index.html'
+		})
+	]
+};
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch( process.env.npm_lifecycle_event ) {
+	// PRODUCTION SET UP
+	case 'prod-build' : 
+		config = merge(common,{});
+		break;
+	// DEV SETUP
+	default:
+		config = merge(common,{});
+}
+
+module.exports = validate(config, {
+	quiet: true
+});
