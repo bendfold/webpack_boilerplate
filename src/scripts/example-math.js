71521a83eeacfcd3796ce6cb76d44f6f7c1f0aa3
owen cowdry <owencowdry@gmail.com>
2017-06-01 11:38:31 +0100

    switched out babel for babel-cli. Sorted out example adder file to use imports rather than require

--- src/scripts/example-math.js
+++ src/scripts/example-math.js
@@ -1,5 +1,7 @@
-exports.adder = a => {
+const adder = a => {
 	return (b) => {
 		return a + b;
 	};
 };
+
+export default adder;
--- test/example-math-test.js
+++ test/example-math-test.js
@@ -1,9 +1,9 @@
-var test = require('tape');
-var exampleMath = require('../src/scripts/example-math.js');
+import test from 'tape';
+import adder from '../src/scripts/example-math.js';
 
-test('exampleMath.add example test', function(t) {
+test('Adder example test', function(t) {
 	t.plan(3);
-	var fivePlus = exampleMath.adder(5);
+	var fivePlus = adder(5);
 	t.equal(fivePlus(5), 10);
 	t.equal(fivePlus(10), 15);
 	t.equal(fivePlus(15), 20);
