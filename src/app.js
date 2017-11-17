753fc5b301875c11aa6739f219380651bcc3ac2f
owen cowdry <owencowdry@gmail.com>
2017-11-17 13:51:00 +0000

    Removing commented out code

--- src/app.js
+++ src/app.js
@@ -2,18 +2,4 @@
 import './styles/app.styl';
 import appInit from './scripts/app-init';
 
-var foo = {
-	a: 1,
-	b: 2,
-	c: 3
-};
-
-var bar = {
-	...foo,
-	x: 4
-};
-
-console.log('foo <<< ', foo);
-console.log('bar >>> ', bar);
-
 appInit();
