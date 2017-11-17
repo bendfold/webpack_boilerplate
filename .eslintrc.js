597cd6667064d19677a31313ed09a9a18ee47271
owen cowdry <owencowdry@gmail.com>
2017-11-17 13:49:59 +0000

    Removing commented out code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,7 @@
-// module.exports = {
-// 	"env": {
-// 		"browser": true,
-// 		"commonjs": true,
-// 		"es6": true,
-// 		"node": true,
-// 	},
-// 	// Extend existing configuration
-// 	// from ESlint and eslint-plugin-react defaults.
-// 	"extends": [
-// 		"eslint:recommended"
-// 	],
-// 	// Enable ES6 support.
-// 	"parserOptions": {
-// 		"ecmaVersion": 6,
-// 		"sourceType": "module"
-// 	},
-// 	"env": {
-// 		"browser": true,
-// 		"node": true
-// 	},
-// 	"rules": {
-// 		// Disable `no-console` rule
-// 		"no-console": 0,
-// 		// Give a warning if identifiers contain underscores
-// 		"no-underscore-dangle": 1,
-// 		// Default to single quotes and raise an error if something
-// 		// else is used
-// 		"quotes": [2, "single"],
-// 		"semi" : [2, "always"],
-
-// 	},
-// };
-
-
+// https://github.com/babel/babel-eslint
 module.exports = {
 	"parser": "babel-eslint",
 	"rules": {
 		"strict": 0
 	}
 };
-
-// https://github.com/babel/babel-eslint
